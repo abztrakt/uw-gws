5733bb482a3695abb7a10115d1bf713c997fc962
Michael Nguyen <mnnguyen@uw.edu>
2011-12-29 16:34:23 -0800

    Added the *.pyc files to files to be ignored by git. Modified the models.py to work properly. Added admin.py so that the admin site can recognize the uw_gws app.

---
+++ admin.py
@@ -0,0 +1,4 @@
+from uw_gws.models import UWGWSGroup
+from django.contrib import admin
+
+admin.site.register(UWGWSGroup)
--- models.py
+++ models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class UWGWSGroup(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
