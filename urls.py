cef9b6171b1eb5ce725339c40702ec96ce9c0edf
Michael Nguyen <mnnguyen@uw.edu>
2011-12-30 13:18:59 -0800

    Fixed urls.py so that it understands the views in the uw_gws app.

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('uw_gws.views',
-    url(r'groups/(?P<group>\w+)/$', 'view_group_info', name='view_group_info'),
+    url(r'groups/(?P<group>.*)/$', 'view_group_info', name='view_group_info'),
 )
