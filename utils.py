25cccac3b84f7a6693c98f287423cd99f859ed00
Michael Nguyen <mnnguyen@uw.edu>
2012-01-06 16:21:13 -0800

    Moved the update functionality from views.py to utils.py. The view still behaves the same as before.

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 import httplib
 from django.conf import settings
+from django.contrib.auth.models import User,Group
 import xml.dom.minidom
 
 def get_group_info(group):
+    '''
+    Returns the information of a group via Groups Web Service in XHTML.
+    @return (dictionary,boolean)
+    '''
     URL = settings.URL + 'group/%s/' % group
 
     connection = httplib.HTTPSConnection(settings.GWS_HOST,settings.GWS_PORT,settings.KEY_FILE,settings.CERT_FILE)
@@ -12,16 +17,17 @@ def get_group_info(group):
     body = response.read()
     connection.close()
 
+    
     if response.status == 404:
-        return ("Error: The group '%s' was not found in the Groups Web Service, instead a 404 error was returned." % group, False)
+        return ({'error':"Error: The group '%s' was not found in the Groups Web Service, instead a 404 error was returned." % group}, False)
 
-    return (body,True)
+    return ({'group_info':body},True)
 
 
 def get_group_members(group):
     '''
     Returns the members of a group via Groups Web Service.
-    @return (status, members)
+    @return (dictionary,boolean)
     '''
 
     URL = settings.URL + 'group/%s/member' % group
@@ -33,7 +39,7 @@ def get_group_members(group):
     connection.close()
      
     if response.status == 404:
-        return ("Error: The group '%s' was not found in the Groups Web Service, instead a 404 error was returned." % group, False)
+        return ({'error':"Error: The group '%s' was not found in the Groups Web Service, instead a 404 error was returned." % group}, False)
 
     # Use XHTML parsing to get the group members.
     group_members_array = []
@@ -45,26 +51,34 @@ def get_group_members(group):
         if (m is not None and m.getAttribute('class') == 'member'):
             group_members_array.append(m.firstChild.data)
 
-    return (group_members_array,True)
+    return ({'group_members':group_members_array},True)
 
 
-def update_members(group):
+def update_group_members(group):
     '''
     Updates the users of a group for use in the django databases.
     '''
     
     # Grab all of the members of the group from the group web service. If the group doesn't exist, stop further processing.
-    result = get_group_members(group)
+    result, group_exists = get_group_members(group)
     
-    if result[1]:
+    if group_exists:
         # If the group exists in the web service, get or create the group.
         django_group,django_group_created = Group.objects.get_or_create(name=group)
 
         # Grab all of the members of the group in django.
         users = sorted([user.username for user in django_group.user_set.all()])
-        result = sorted(result[0])
+        result = sorted(result['group_members'])
 
-        if users != result:
+        # Primarily used for testing, these lists will be populated with any updates to the database regarding groups.
+        current_users = []
+        created_users = []
+        removed_users = []
+        added_users = []
+
+        if users == result:
+            current_users = users
+        else:
             # Grab or create the users from the group and add them to the group.
             for member in result:
                 user,user_created = User.objects.get_or_create(username=member)
@@ -74,7 +88,13 @@ def update_members(group):
                     user.is_staff = False
                     user.is_superuser = False
                     user.save()
-                
+                    created_users.append(user)
+
+                if user.username in users:
+                    current_users.append(user)
+                elif not user_created and user.username not in users:
+                    added_users.append(user)
+
                 # Now add them to the group.
                 user.groups.add(django_group)
                 user.save()
@@ -85,3 +105,13 @@ def update_members(group):
                 user = User.objects.get(username=member)
                 user.groups.remove(django_group)
 
+        result = {
+            'current_users': current_users,
+            'created_users': created_users,
+            'removed_users': removed_users,
+            'added_users': added_users,
+        }
+
+        return result
+    else:
+        return result['error']
--- views.py
+++ views.py
@@ -18,16 +18,16 @@ def view_group_info(request,group):
     '''
     Returns all of the HTML information associated with that group. 
     '''
-    result = utils.get_group_info(group)
+    result, group_exists = utils.get_group_info(group)
     args = {
         'title': 'Group: %s' % group, 
         'group': group,
     }
 
-    if not result[1]:
-        args['error'] = result[0] + " No group info can be provided." 
+    if not group_exists:
+        args['error'] = result['error']+ " No group info can be provided." 
     else:
-        args['result'] = result[0]
+        args['result'] = result['group_info']
 
     return render_to_response('group.html',args,context_instance=RequestContext(request))
 
@@ -49,17 +49,17 @@ def view_group_members(request,group):
     Returns a list of all of the members of the group using the Groups Web Service.
     '''
 
-    result = utils.get_group_members(group)
+    result,group_exists = utils.get_group_members(group)
     
     args = {
         'title': 'Group: %s - Members' % group, 
         'group': group,
     }
 
-    if not result[1]:
-        args['error'] = result[0] + " No members to list." 
+    if not group_exists:
+        args['error'] = result['error'] + " No members to list." 
     else:
-        args['result'] = result[0]
+        args['result'] = result['group_members']
 
     return render_to_response('members.html',args,context_instance=RequestContext(request))
 
@@ -68,70 +68,23 @@ def update_members(request,group):
     Updates the users of a group for use in the django databases.
     '''
     
+    # Call the update_group_members method in utils.py to update the groups.
+    args = utils.update_group_members(group)
+
     # Grab all of the members of the group from the group web service. If the group doesn't exist, stop further processing.
-    result = utils.get_group_members(group)
+    result, group_exists = utils.get_group_members(group)
     
-    if not result[1]:
+    if not group_exists:
         # No group was found. Return a error message. 
         args = {
             'title':'Group: %s - Update members' % group,
             'group': group,
-            'error': result[0] + " No group update was performed.",
+            'error': result['error'] + " No group update was performed.",
         }
         return render_to_response('update.html',args,context_instance=RequestContext(request))
 
-    # If the group exists in the web service, get or create the group.
-    django_group,django_group_created = Group.objects.get_or_create(name=group)
-
-    # Grab all of the members of the group in django.
-    users = sorted([user.username for user in django_group.user_set.all()])
-    result = sorted(result[0])
-
-    # Primarily used for testing, these lists will be populated with any updates to the database regarding groups.
-    current_users = []
-    created_users = []
-    removed_users = []
-    added_users = []
-
-    if users == result:
-        # We have nothing to update.
-        current_users = users
-    else:
-        # Grab or create the users from the group and add them to the group.
-        for member in result:
-            user,user_created = User.objects.get_or_create(username=member)
-            
-            if user_created:
-                # TODO: Check to see how to deal with default permissions.
-                # Create the user. Set default permissions.
-                user.is_staff = False
-                user.is_superuser = False
-                user.save()
-                created_users.append(user)
-            
-            if user.username in users:
-                current_users.append(user)
-            elif not user_created and user.username not in users:
-                added_users.append(user)
-
-            # Now add them to the group.
-            user.groups.add(django_group)
-            user.save()
-
-        #Now check which people weren't in the group and remove them.
-        removed_users = [user for user in users if user not in result]
-        for member in removed_users:
-            user = User.objects.get(username=member)
-            user.groups.remove(django_group)
-
-    args = {
-        'title':'Group: %s - Update members' % group,
-        'group': group,
-        'current_users': current_users,
-        'created_users': created_users,
-        'removed_users': removed_users,
-        'added_users': added_users,
-    }
-
+    args['group'] = group
+    args['title'] = 'Group: %s - Update members' % group,
+ 
     return render_to_response('update.html',args,context_instance=RequestContext(request))
     
