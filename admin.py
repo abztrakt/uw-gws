2f08f4c54ba10112785adb9554e4271350e2b52f
Michael Nguyen <mnnguyen@uw.edu>
2012-01-09 16:28:58 -0800

    Added the ability to update a group through the admin interface. Fixes #105

--- admin.py
+++ admin.py
@@ -1,2 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
+# Utils.py contains the get request needed to process the information.
+import utils
+
+
+# Modify the actions available for groups in the admin interface.
+class GroupAdmin(admin.ModelAdmin):
+    actions = ['update_group']
+    def update_group(self,request,queryset):
+        count = 0
+        for group in queryset:
+            update = utils.update_group_members(group.name)
+            result, group_exists = utils.get_group_members(group.name)
+            if group_exists:
+                count += 1
+
+        if count == 0:
+            message_bit = "No groups were"
+        elif count == 1:
+            message_bit = "1 group was"
+        else:
+            message_bit = "%d groups were" % (count)
+
+        self.message_user(request, "%s successfully updated." % message_bit)
+    update_group.short_description = "Update selected groups"
+
+admin.site.unregister(Group)
+admin.site.register(Group,GroupAdmin)
