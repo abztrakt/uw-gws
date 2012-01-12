a46f94f0a5038cab8f1f75ff2158eaa3db977ea6
Michael Nguyen <mnnguyen@uw.edu>
2012-01-12 13:01:51 -0800

    Updated the members.py command to include a count of groups that updated, were unaffected, or failed to update. Also included the option to include django's '--verboisty' flag to handle the amount of output to be shown after an update command.
    Examples:
    Input : ./manage.py members --verbosity 0
    Output: <NO OUTPUT>
    
    Input : ./manage.py members OR ./members.py members --verbosity 1
    Output: Starting update...
            Update finished. Updated: 1, Unaffected: 1, Failed: 1
    
    Input : ./manage.py members --verbosity 2
    Output: Starting update...
            Successfully updated group "u_cac_tm-aca". Created: 3, Added: 0, Removed: 0
            No update needed for group "u_mnnguyen_django_test".
            Group "test" cannot be found via Groups Web Service. Update failed.
            Update finished. Updated: 1, Unaffected: 1, Failed: 1

--- management/commands/members.py
+++ management/commands/members.py
@@ -4,11 +4,22 @@ import uw_gws.utils as utils
 
 class Command(BaseCommand):
     args = ''
-    help = 'Updates all of the auth.groups via Groups Web Service'
+    help = 'Updates all of the auth.groups membership via Groups Web Service'
 
     def handle(self, *args, **options):
         groups = Group.objects.all()
-        self.stdout.write('Starting update...\n')
+        
+        #Determine how verbose the output should be in the command line.
+        verbosity = int(options['verbosity'])
+
+        if verbosity > 0:
+            self.stdout.write('Starting update...\n')
+            
+        #Count how many updates were performed.
+        updated_count = 0
+        unaffect_count = 0
+        failed_count = 0
+
         for group in groups:
             result = utils.update_group_members(group.name)
 
@@ -23,11 +34,16 @@ class Command(BaseCommand):
                 
                 if not created and not removed and not added:
                     message = 'No updated needed for group "%s".' % (group.name) 
+                    unaffect_count += 1
                 else:
                     message = 'Successfully updated group "%s". Created: %d, Added:%d, Removed: %d.' % (group.name, len(created),len(added),len(removed))
+                    updated_count += 1
             else:
                 message = 'Group "%s" cannot be found via Group Web Service. Update failed.' % (group.name)
+                failed_count += 1
 
-            self.stdout.write('%s\n' % message)
-        
-        self.stdout.write('Update finished.\n')
+            if verbosity > 1:
+                self.stdout.write('%s\n' % message)
+
+        if verbosity > 0:
+            self.stdout.write('Update finished. Updated: %d, Unaffected: %d, Failed: %d\n' % (updated_count, unaffect_count, failed_count) )
